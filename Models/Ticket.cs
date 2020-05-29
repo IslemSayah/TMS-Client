03aa1951f331cba9404a4918495d0351ba5bcedb
IslemSayah <fi_sayah@esi.dz>
2020-05-29 13:13:12 +0000

    publish

---
+++ Models/Ticket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TMS_Client.Models
+{
+    public class Ticket
+    {
+        public Guid Id { get; set; }
+    }
+}
