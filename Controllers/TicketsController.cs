e580bc2e63ddf0e9eac7c566898645fedd7d192f
IslemSayah <fi_sayah@esi.dz>
2020-05-31 10:23:26 +0100

    FewUpdates For Testing

---
+++ Controllers/TicketsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMS_Client.Controllers
+{
+    public class TicketsController : Controller
+    {
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
